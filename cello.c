5efdad30d4df49e2fed1682f1a6876b093745fd0
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2024-12-14 18:17:39 +0800

    feat(ch2.1): Implement cello program
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

---
+++ cello.c
@@ -0,0 +1,13 @@
+/*
+    Output `Hello World` on the command line
+
+    Copyright © 2024 Red Hat, Inc.
+    Copyright © 2024 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
+    SPDX-License-Identifier: CC-BY-SA-3.0
+*/
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+    return 0;
+}
